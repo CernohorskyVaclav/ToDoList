bcd4862cc1b1ee684e81cf03e1fa05087ebf24fd
CernohorskyVaclav <vaclav.cernohorsky@gmail.com>
2019-12-02 20:48:24 +0100

    Initial Commit

---
+++ ToDoListTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListTwo
+//
+//  Created by Václav Černohorský on 02/12/2019.
+//  Copyright © 2019 Václav Černohorský. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
